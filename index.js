ad0e746dc0a5966140b13532213c687b8f1e236a
Mike Taylor <mike@indexdata.com>
2017-05-03 17:39:57 +0100

    Move settings down into 'settings' directory'

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-// We have to remove node_modules/react to avoid having multiple copies loaded.
-// eslint-disable-next-line import/no-unresolved
-import React from 'react';
-
-export default () => <div>Organization-level settings (to be done)</div>;
+export { default } from './settings';
