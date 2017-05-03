f163e693e3fe377696f8abe71e6194d2d1cbd202
Mike Taylor <mike@indexdata.com>
2017-05-03 17:42:58 +0100

    Top-level settings page uses stripes-components Settings

--- settings/index.js
+++ settings/index.js
@@ -1,5 +1,16 @@
 // We have to remove node_modules/react to avoid having multiple copies loaded.
 // eslint-disable-next-line import/no-unresolved
 import React from 'react';
+import Settings from '@folio/stripes-components/lib/Settings';
 
-export default () => <div>No organization-level settings yet</div>;
+import Locale from './Locale';
+
+const pages = [
+  {
+    route: 'locale',
+    label: 'Locale',
+    component: Locale,
+  },
+];
+
+export default props => <Settings {...props} pages={pages} />;
