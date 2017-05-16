01fbbeb5b74572a5c6c09735fa91d7e16c42f7c2
Mike Taylor <mike@indexdata.com>
2017-05-16 17:47:00 +0100

    Link through to detailed Module pages
    Part of STRIPES-378.

--- settings/About.js
+++ settings/About.js
@@ -3,38 +3,54 @@
 import React, { PropTypes } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Pane from '@folio/stripes-components/lib/Pane';
-import Select from '@folio/stripes-components/lib/Select';
+import Route from 'react-router-dom/Route';
+import Link from 'react-router-dom/Link';
+import Module from './Module';
 
 
 class About extends React.Component {
   static propTypes = {
     data: PropTypes.shape({
       modules: PropTypes.arrayOf(
-        PropTypes.object
+        PropTypes.object,
       ),
+    }).isRequired,
+    match: PropTypes.shape({
+      path: PropTypes.string.isRequired,
     }).isRequired,
   };
 
   static manifest = Object.freeze({
     modules: {
       type: 'okapi',
-      path: '_/proxy/modules?full=true'
+      path: '_/proxy/modules',
     },
   });
 
   render() {
+    const modules = this.props.data.modules || [];
+
     return (
       <Pane defaultWidth="fill" fluidContentWidth paneTitle="About">
         <Row>
           <Col xs={12}>
             <label htmlFor="setting">Back-end modules available</label>
-        <ul>
-        {
-          (this.props.data.modules || []).map(m => <li key={m.id}><b>{m.id}</b> ({m.name})</li>)
-        }
-        </ul>
+            <ul>
+              {
+                modules.map(m => (
+                  <li key={m.id}>
+                    <Link
+                      key={m.route}
+                      to={`${this.props.match.path}/${m.id}`}
+                    ><b>{m.id}</b> ({m.name})
+                    </Link>
+                  </li>),
+                )
+              }
+            </ul>
           </Col>
         </Row>
+        <Route path={`${this.props.match.path}/:id`} component={Module} />
       </Pane>
     );
   }
