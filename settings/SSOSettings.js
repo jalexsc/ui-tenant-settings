5ce42b29252df36db3a911eb9ee47498bc5f2411
Zak Burke <zburke@cornell.edu>
2017-10-12 16:43:18 -0400

    Import PropTypes from prop-types, not React. Refs STRIPES-427

--- settings/SSOSettings.js
+++ settings/SSOSettings.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Row, Col } from 'react-bootstrap';
 import Pane from '@folio/stripes-components/lib/Pane';
 import TextField from '@folio/stripes-components/lib/TextField';
