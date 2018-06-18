813a2e032f6af4482789527dd18e9b6318156bf3
Zak Burke <zburke@cornell.edu>
2018-06-18 15:08:01 -0400

    add location management tests

---
+++ test/ui-testing/test.js
@@ -0,0 +1,19 @@
+/*
+const newUser = require('./new_user.js');
+const patronGroup = require('./patron_group.js');
+const newPermissionSet = require('./new_permission_set.js');
+const newProxy = require('./new_proxy.js');
+
+module.exports.test = function meh(uitestctx) {
+  patronGroup.test(uitestctx);
+  newUser.test(uitestctx);
+  newPermissionSet.test(uitestctx);
+  newProxy.test(uitestctx);
+};
+*/
+
+const locations = require('./locations.js');
+
+module.exports.test = function meh(uitestctx) {
+  locations.test(uitestctx);
+};
