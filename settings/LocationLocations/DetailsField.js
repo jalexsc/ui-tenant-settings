da722263c227a4690dd5af068b969a8ebaaae286
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-10-04 10:51:03 -0500

    Use upstream AutoSuggest

--- settings/LocationLocations/DetailsField.js
+++ settings/LocationLocations/DetailsField.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TextField from '@folio/stripes-components/lib/TextField';
+import { AutoSuggest, TextField } from '@folio/stripes-components';
 import RepeatableField from '../../lib/RepeatableField';
-import AutoSuggest from '../../lib/AutoSuggest';
 
 class DetailsField extends React.Component {
   static manifest = {
