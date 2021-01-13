93a62c43c69060b3a6435f6efd907afdf380111a
Alex Mozolev <aleksey_mozolev@epam.com>
2021-01-13 20:01:42 +0300

    UITEN-119 follow-up fix naming

--- src/settings/LocationLocations/LocationForm/RemoteStorageField.js
+++ src/settings/LocationLocations/LocationForm/RemoteStorageField.js
@@ -3,7 +3,7 @@ import { useField } from 'react-final-form';
 
 import { Control, useRemoteStorageApi } from '../RemoteStorage';
 
-export const RemoteStorageField = ({ parentMutator, initialValues, checkLocationHasHoldingsOrItems }) => {
+export const RemoteStorageField = ({ initialValues, checkLocationHasHoldingsOrItems }) => {
   const { remoteMap, mappings, t } = useRemoteStorageApi();
 
   const [isReadOnly, setIsReadOnly] = React.useState(true);
@@ -16,18 +16,18 @@ export const RemoteStorageField = ({ parentMutator, initialValues, checkLocation
     () => {
       // For new locations - initial state is NOT read-only
       if (locationId === undefined) {
-        setReadOnly(false);
+        setIsReadOnly(false);
         return;
       }
 
       // Initial state is read-only, until we get assured there are no Holdings or Items associated
-      setReadOnly(true);
-      checkLocationHasHoldingsOrItems(locationId).then(setReadOnly);
+      setIsReadOnly(true);
+      checkLocationHasHoldingsOrItems(locationId).then(setIsReadOnly);
     },
     [locationId]
   );
 
-  const message = (mappings.failed && t`failed`) || (mappings.isPending && t`loading`) || (readOnly && t`readonly`);
+  const message = (mappings.failed && t`failed`) || (mappings.isPending && t`loading`) || (isReadOnly && t`readonly`);
 
   const isDisabled = !mappings.hasLoaded;
 
@@ -36,7 +36,7 @@ export const RemoteStorageField = ({ parentMutator, initialValues, checkLocation
       label={t`remote`}
       required
       disabled={isDisabled}
-      readOnly={readOnly && !isDisabled}
+      readOnly={isReadOnly && !isDisabled}
       message={message}
       {...field}
     />
